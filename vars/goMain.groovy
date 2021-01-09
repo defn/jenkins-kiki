84ce67b9abf4d87af6ff1bad13e906c204eba776
Amanibhavam <iam@defn.sh>
2021-01-08 21:33:44 -0800

    accept a body

--- vars/goMain.groovy
+++ vars/goMain.groovy
@@ -1,4 +1,4 @@
-def call(pipelineRoleId, jenkinsSecrets, pipelineSecrets, nmJob, nmBinary, nmDocker, vendorPrefix) {
+def call(pipelineRoleId, jenkinsSecrets, pipelineSecrets, nmJob, nmBinary, nmDocker, vendorPrefix, Closure body) {
   goPrep()
 
   withCredentials([[
@@ -6,28 +6,24 @@ def call(pipelineRoleId, jenkinsSecrets, pipelineSecrets, nmJob, nmBinary, nmDoc
     credentialsId: 'VaultToken',
     vaultAddr: env.VAULT_ADDR ]]) {
 
-    stage ('Secrets') {
-      def PIPELINE_SECRET_ID= ''
-      env.PIPELINE_SECRET_ID = sh(returnStdout: true, script: "./ci/build ${nmJob}").trim()
-
-      def pipelineConfiguration = creds(pipelineRoleId, env.PIPELINE_SECRET_ID)
-
-      withVault([vaultSecrets: pipelineSecrets, configuration: pipelineConfiguration]) {
-        sh("env | grep MEH")
-      }
-    }
+    def PIPELINE_SECRET_ID= ''
+    env.PIPELINE_SECRET_ID = sh(returnStdout: true, script: "./ci/build ${nmJob}").trim()
+    def pipelineConfiguration = creds(pipelineRoleId, env.PIPELINE_SECRET_ID)
 
     withVault([vaultSecrets: jenkinsSecrets]) {
-      withEnv(["DOCKER_CONFIG=/tmp/docker/${env.BUILD_TAG}"]) {
-        if (env.TAG_NAME) {
-          goRelease()
-
-          stage('Test Docker image') {
-            sh "/env.sh docker run --rm --entrypoint /" + nmBinary + "  " + nmDocker + ":" + vendorPrefix + "${env.GORELEASER_CURRENT_TAG.minus('v')}-amd64"
+      withVault([vaultSecrets: pipelineSecrets, configuration: pipelineConfiguration]) {
+        withEnv(["DOCKER_CONFIG=/tmp/docker/${env.BUILD_TAG}"]) {
+          if (env.TAG_NAME) {
+            goRelease()
+            body()
+            stage('Test Docker image') {
+              sh "/env.sh docker run --rm --entrypoint /" + nmBinary + "  " + nmDocker + ":" + vendorPrefix + "${env.GORELEASER_CURRENT_TAG.minus('v')}-amd64"
+            }
           }
-        }
-        else {
-          goBuild()
+          else {
+            goBuild()
+            body()
+          }
         }
       }
     }
