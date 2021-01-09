4970f5fa2fc7c6a8afaf8cfbb737e734bb3c9a06
Amanibhavam <iam@defn.sh>
2021-01-08 21:35:38 -0800

    dont need a few vars

--- vars/goMain.groovy
+++ vars/goMain.groovy
@@ -1,4 +1,4 @@
-def call(pipelineRoleId, jenkinsSecrets, pipelineSecrets, nmJob, nmBinary, nmDocker, vendorPrefix, Closure body) {
+def call(pipelineRoleId, jenkinsSecrets, pipelineSecrets, nmJob, Closure body) {
   goPrep()
 
   withCredentials([[
@@ -16,9 +16,6 @@ def call(pipelineRoleId, jenkinsSecrets, pipelineSecrets, nmJob, nmBinary, nmDoc
           if (env.TAG_NAME) {
             goRelease()
             body()
-            stage('Test Docker image') {
-              sh "/env.sh docker run --rm --entrypoint /" + nmBinary + "  " + nmDocker + ":" + vendorPrefix + "${env.GORELEASER_CURRENT_TAG.minus('v')}-amd64"
-            }
           }
           else {
             goBuild()
