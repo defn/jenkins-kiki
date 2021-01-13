4005e3eef5f5a8182f4c2a52b660439c8979ca8c
Amanibhavam <iam@defn.sh>
2021-01-12 18:11:15 -0800

    group figlet with the stage

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,7 +1,6 @@
 def call() {
-  sh("/env.sh figlet -f /j/chunky.flf build")
-
   stage('Build') {
+    sh("/env.sh figlet -f /j/chunky.flf build")
     sh "/env.sh goreleaser build --rm-dist"
   }
 }
--- vars/goRelease.groovy
+++ vars/goRelease.groovy
@@ -1,7 +1,6 @@
 def call() {
-  sh("/env.sh figlet -f /j/chunky.flf release")
-
   stage('Release') {
+    sh("/env.sh figlet -f /j/chunky.flf release")
     sh "install -d -m 0700 /tmp/docker"
     sh "install -d -m 0700 /tmp/docker/${env.BUILD_TAG}"
     sh "env | grep ^DOCKER_PASSWORD= | cut -d= -f2- | docker login --password-stdin --username ${DOCKER_USERNAME}"
