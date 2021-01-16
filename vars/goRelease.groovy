0cc7b2986628cf544ed2cf652317767c91439cc4
Amanibhavam <iam@defn.sh>
2021-01-15 22:05:22 -0800

    no docker login

--- vars/goRelease.groovy
+++ vars/goRelease.groovy
@@ -2,9 +2,6 @@ def call() {
   stage('Release') {
     sh """
       /env.sh figlet -f /j/chunky.flf release
-      install -d -m 0700 /tmp/docker
-      install -d -m 0700 /tmp/docker/${env.BUILD_TAG}
-      env | grep ^DOCKER_PASSWORD= | cut -d= -f2- | docker login --password-stdin --username ${DOCKER_USERNAME}
       /env.sh /app/src/bin/goreleaser release --rm-dist
     """
   }
