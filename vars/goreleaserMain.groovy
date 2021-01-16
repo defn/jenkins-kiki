b38a740ff1603c0a3af4f68241036e69488fb496
Amanibhavam <iam@defn.sh>
2021-01-15 16:07:52 -0800

    try finally

--- vars/goreleaserMain.groovy
+++ vars/goreleaserMain.groovy
@@ -1,45 +1,55 @@
 def call(Map param, Closure body) {
   node() {
-    goPrep()
+    try {
+      goPrep()
 
-    withCredentials([[
-      $class: 'VaultTokenCredentialBinding',
-      credentialsId: 'VaultToken',
-      vaultAddr: env.VAULT_ADDR ]]) {
+      withCredentials([[
+        $class: 'VaultTokenCredentialBinding',
+        credentialsId: 'VaultToken',
+        vaultAddr: env.VAULT_ADDR ]]) {
 
-      sh("/env.sh figlet -f /j/chunky.flf secrets")
+        sh("/env.sh figlet -f /j/chunky.flf secrets")
 
-      def WRAPPED_SID = ''
-      env.WRAPPED_SID = sh(returnStdout: true, script: "/env.sh vault write -field=wrapping_token -wrap-ttl=60s -f auth/approle/role/${param.role}/secret-id").trim()
-    
-      def UNWRAPPED_SID = ''
-      env.UNWRAPPED_SID= sh(returnStdout: true, script: 'set +x; /env.sh vault unwrap -field=secret_id ${WRAPPED_SID}; set -x').trim()
+        def WRAPPED_SID = ''
+        env.WRAPPED_SID = sh(returnStdout: true, script: "/env.sh vault write -field=wrapping_token -wrap-ttl=60s -f auth/approle/role/${param.role}/secret-id").trim()
+      
+        def UNWRAPPED_SID = ''
+        env.UNWRAPPED_SID= sh(returnStdout: true, script: 'set +x; /env.sh vault unwrap -field=secret_id ${WRAPPED_SID}; set -x').trim()
 
-      def pipelineConfiguration = creds(param.roleId, env.UNWRAPPED_SID)
+        def pipelineConfiguration = creds(param.roleId, env.UNWRAPPED_SID)
 
-      def jenkinsSecrets = [
-        path: 'kv/jenkins/common',
-        secretValues: [
-          [vaultKey: 'GITHUB_TOKEN'],
-          [vaultKey: 'DOCKER_USERNAME'],
-          [vaultKey: 'DOCKER_PASSWORD']
+        def jenkinsSecrets = [
+          path: 'kv/jenkins/common',
+          secretValues: [
+            [vaultKey: 'GITHUB_TOKEN'],
+            [vaultKey: 'DOCKER_USERNAME'],
+            [vaultKey: 'DOCKER_PASSWORD']
+          ]
         ]
-      ]
 
-      withVault([vaultSecrets: param.pipelineSecrets + jenkinsSecrets, configuration: pipelineConfiguration]) {
-        withEnv(["DOCKER_CONFIG=/tmp/docker/${env.BUILD_TAG}"]) {
-          if (env.TAG_NAME) {
-            goRelease()
-            body()
-          }
-          else {
-            goBuild()
-            body()
+        withVault([vaultSecrets: param.pipelineSecrets + jenkinsSecrets, configuration: pipelineConfiguration]) {
+          withEnv(["DOCKER_CONFIG=/tmp/docker/${env.BUILD_TAG}"]) {
+            if (env.TAG_NAME) {
+              goRelease()
+              body()
+            }
+            else {
+              goBuild()
+              body()
+            }
           }
         }
       }
-    }
 
-    goClean()
+      goClean()
+    }
+  }
+  finally {
+    if (success) {
+      sh("/env.sh figlet -f /j/chunky.flf success")
+    }
+    else {
+      sh("/env.sh figlet -f /j/chunky.flf fail")
+    }
   }
 }
